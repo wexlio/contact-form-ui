1cdd089b44e983e760a9dcbef0020fe9c58b70f5
Diego A. Espinoza Reyna <108908454+wexlio@users.noreply.github.com>
2022-10-28 09:44:21 -0500

    Update codigo2.js

--- codigo2.js
+++ codigo2.js
@@ -1,3 +1,3 @@
-setTimeout (function(){
-    window.location.href = "https://api.whatsapp.com/send?phone=+51992830820&text=Hola! 👋 %20Me%20interesa%20el%20terreno%20en%20carabayllo%20de%20587m2.%20Acabo%20de%20rellenar%20el%20formulario.%20Espero%20su%20respuesta."
-}, 1500);
+//setTimeout (function(){
+   // window.location.href = "https://api.whatsapp.com/send?phone=+51992830820&text=Hola! 👋 %20Me%20interesa%20el%20terreno%20en%20carabayllo%20de%20587m2.%20Acabo%20de%20rellenar%20el%20formulario.%20Espero%20su%20respuesta."
+//}, 1500);
