07fd165539281fbcd99bf52a82e0e2f98b50d202
Diego A. Espinoza Reyna <108908454+wexlio@users.noreply.github.com>
2022-10-28 12:04:44 -0500

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,37 +1,3 @@
-
-const mapDiv = document.getElementById("map");
-let map;
-const cordenada = { lat: -11.831044, lng:  -77.043606 };
-var citymap = {
-    Carabayllo: {
-        center: {lat: -11.831044, lng: -77.043606},
-        population: 200
-    }
-    };
-
-async function iniciarMap() {
-    map = await new google.maps.Map(mapDiv, {
-        center: cordenada,
-        scaleControl: true,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 15,
-    });
-    for (var city in citymap) {
-            var cityCircle = new google.maps.Circle({
-            strokeColor: '#0000FF',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: '#5882FA',
-            fillOpacity: 0.35,
-            map,
-            center: citymap[city].center,
-            radius: Math.sqrt(citymap[city].population) * 10
-        });
-    }
-}
-
-iniciarMap();
-
 const form = document.getElementById("form");
 
 const nombre = document.getElementById("nombre");
