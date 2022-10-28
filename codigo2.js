7a807ca10645752762175d7d64c50ff5afacd64d
Diego A. Espinoza Reyna <108908454+wexlio@users.noreply.github.com>
2022-10-28 10:58:32 -0500

    Update codigo2.js

--- codigo2.js
+++ codigo2.js
@@ -18,7 +18,7 @@ async function iniciarMap() {
         center: cordenada,
         scaleControl: true,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 12.9,
+        zoom: 13,
     });
     for (var city in citymap) {
             var cityCircle = new google.maps.Circle({
